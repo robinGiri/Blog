8df5b6e2f20657eeee264f01057b06446b0cbe38
robinGiri <girirobin2018@gmail.com>
2020-08-17 19:16:27 +0545

    news app added in portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
   portfolio = [];
@@ -14,15 +13,17 @@ export class PortfolioComponent implements OnInit {
       {
         icon: 'assets/img/planet_home.png',
         title: "Planet Learning",
+        url:"https://github.com/open-learning-exchange/planet",
         info:
           "Planet Learning is a generic learning system built in Angular & CouchDB."
       },
-      // {
-      //   icon: '',
-      //   title: "Endless Hiking",
-      //   info:
-      //     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias molestias eius libero?"
-      // },
+      {
+        icon: 'assets/img/newsapp.jpg',
+        title: "News App",
+        url:"https://github.com/robinGiri/news",
+        info:
+          "News App is a simple mobile application built in Flutter project."
+      },
       // {
       //   icon: '',
       //   title: "Free Shuttle",
