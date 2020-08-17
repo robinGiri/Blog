3c05f30abf35f1d4b3e0f25b1f8e94aa0262a3cb
robinGiri <girirobin2018@gmail.com>
2020-08-17 12:19:22 +0545

    navigation and router link set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { InterestsComponent } from './interests/interests.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: NavigationComponent },
+  { path: 'aboutMe', component: AboutMeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'interests', component: InterestsComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
