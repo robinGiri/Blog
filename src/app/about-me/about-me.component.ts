79e17eed642fe49c7e303ae7f03be88f73ffded2
robinGiri <girirobin2018@gmail.com>
2020-08-18 11:26:17 +0545

    typewriter effect added

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -5,10 +5,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-me.component.html',
 })
 export class AboutMeComponent implements OnInit {
-
+  typewriter_text: string = "Thank you for your interest";
+  typewriter_display: string = "";
   constructor() { }
 
   ngOnInit(): void {
+    this.typingCallback(this);
   }
 
+  typingCallback(that) {
+    let total_length = that.typewriter_text.length;
+    let current_length = that.typewriter_display.length;
+    if (current_length < total_length) {
+      that.typewriter_display += that.typewriter_text[current_length];
+      setTimeout(that.typingCallback, 100, that);
+    } else {
+      that.typewriter_display = "";
+    }
+  }
 }
