c9684c1acbc92784ab4668c2efb6ccd016bf1be1
robinGiri <girirobin2018@gmail.com>
2020-08-21 10:45:12 +0545

    started working on forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormGroup , FormControl , ReactiveFormsModule , FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +28,9 @@ import { BlogComponent } from './blog/blog.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  editForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.userData();
+  }
 
   ngOnInit(): void {
+  }
+
+  userData() {
+    this.editForm = this.fb.group({
+      name: [ '', this.required],
+      subject: [ '',  this.required] ,
+      email: [ '', this.required],
+      message: [ '', this.required],
+    });
+  }
+
+  onSubmit() {
+    if (!this.editForm.valid) {
+      this.showFormErrors(this.editForm.controls);
+      return;
+    }
+    console.log('hay')
+    this.editForm.reset();
+  }
+
+  showFormErrors = (controls: { [key: string]: AbstractControl }) => {
+    Object.values(controls).forEach(control => {
+      control.markAsTouched({ onlySelf: true });
+    });
+  };
+
+  required(ac: AbstractControl) {
+    return /\S/.test(ac.value) ? null : { 'required': true };
   }
 
 }
