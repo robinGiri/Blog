8a185040a31a7595500df54dfd5c7efb32b7060a
robinGiri <girirobin2018@gmail.com>
2020-08-24 19:10:15 +0545

    refactor codes and remove redundent codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormGroup , FormControl , ReactiveFormsModule , FormsModule } from '@angular/forms';
-
-
-import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { InterestsComponent } from './interests/interests.component';
@@ -17,7 +13,6 @@ import { BlogComponent } from './blog/blog.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     AboutMeComponent,
     PortfolioComponent,
     InterestsComponent,
@@ -28,7 +23,6 @@ import { BlogComponent } from './blog/blog.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule,
     FormsModule
   ],
