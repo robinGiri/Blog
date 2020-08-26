94e9777b92913fc53aa03d9d55af96ed40b923a8
robinGiri <girirobin2018@gmail.com>
2020-08-26 23:37:12 +0545

    little animation in action

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
   typewriter_text: string = "Thank you for your interest";
